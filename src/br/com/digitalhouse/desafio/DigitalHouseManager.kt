f9351c1332cc25f53683df1e70e5491923b7071c
Victor Rubens <victorrss@live.com>
2020-09-27 19:22:00 -0300

    Parte G - Classe Manager

---
+++ src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package br.com.digitalhouse.desafio
+
+class DigitalHouseManager() {
+    val alunos = mutableListOf<Aluno>()
+    val professores = mutableListOf<Professor>()
+    val cursos = mutableListOf<Curso>()
+    val matriculas = mutableListOf<Matricula>()
+}
--- src/br/com/digitalhouse/desafio/Matricula.kt
+++ src/br/com/digitalhouse/desafio/Matricula.kt
@@ -2,6 +2,17 @@ package br.com.digitalhouse.desafio
 
 import java.util.*
 
-class Matricula(val aluno: Aluno, val curso: Curso) {
+class Matricula(
+        val codigo: Int,
+        val aluno: Aluno,
+        val curso: Curso) {
     val dtMatricula = Date()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Matricula) return false
+
+        if (codigo != other.codigo) return false
+
+        return true
+    }
 }
