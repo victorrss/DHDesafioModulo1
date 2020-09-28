5a75da6ea987bdd00c47eb1f14db67f93f1e7512
Victor Rubens <victorrss@live.com>
2020-09-27 21:57:55 -0300

    Parte H - Métodos adicionarUmAluno e excluirAluno na classe Curso

--- src/br/com/digitalhouse/desafio/Curso.kt
+++ src/br/com/digitalhouse/desafio/Curso.kt
@@ -7,6 +7,19 @@ abstract class Curso(val codigo: Int,
                      val qtdMaxAlunos: Int) {
     val alunos = mutableListOf<Aluno>()
 
+    fun adicionarUmAluno(aluno: Aluno): Boolean {
+        if (alunos.contains(aluno)) {
+            alunos.add(aluno)
+            return true
+        }
+
+        return false
+    }
+
+    fun excluirAluno(aluno: Aluno){
+        alunos.remove(aluno)
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Curso) return false
